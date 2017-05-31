ada51882745e7caf1a3207ddf641b8c21a98eec2
Hamed Ahmadi <hamed@zerocater.com>
2017-05-31 15:03:21 -0700

    0.8

--- setup.py
+++ setup.py
@@ -6,16 +6,17 @@ from setuptools import setup
 
 setup(
     name='pg_events',
-    version='0.0.7',
+    version='0.0.8',
     description="Postgres events",
     long_description='',
     keywords='postgres python trigger listen notify events',
     author='ZeroCater',
     author_email='tech@zerocater.com',
     url='https://github.com/ZeroCater/pg_events',
-    download_url='https://github.com/ZeroCater/pg_events/tarball/0.0.7',
+    download_url='https://github.com/ZeroCater/pg_events/tarball/0.0.8',
     license='MIT',
     packages=setuptools.find_packages(),
+    package_data={'pg_events': ['core/schemas/*.sql']},
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
