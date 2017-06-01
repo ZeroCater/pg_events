8a28a258caf75cf232fbd202ddd99fd9c9e59554
Hamed Ahmadi <hamed@zerocater.com>
2017-05-31 18:03:51 -0700

    Add auto-build flag

--- pg_events/core/build.py
+++ pg_events/core/build.py
@@ -8,6 +8,11 @@ log = logging.getLogger(__name__)
 
 class Build(Command):
 
+    def __init__(self, args):
+        super(Build, self).__init__(args)
+
+        self.auto_rebuild = args.auto_rebuild or True
+
     def execute(self):
         conn = self.establish_connection()
         conn.autocommit = True
@@ -19,8 +24,9 @@ class Build(Command):
         schema_path = os.path.join(path, 'schemas/triggers.sql')
         cursor.execute(open(schema_path, 'r').read())
 
-        schema_path = os.path.join(path, 'schemas/event_triggers.sql')
-        cursor.execute(open(schema_path, 'r').read())
+        if self.auto_rebuild:
+            schema_path = os.path.join(path, 'schemas/event_triggers.sql')
+            cursor.execute(open(schema_path, 'r').read())
 
         cursor.execute('SELECT pgevents_create_db_triggers();')
         log.info('Creating triggers done.')
